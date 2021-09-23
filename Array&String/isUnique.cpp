e1c7ba448ff85fa8a8e864c88668250c62f98892
naveen-droid <naveen.cs18@nitp.ac.in>
2021-09-23 15:41:54 +0530

    isUnique update

--- Array&String/isUnique.cpp
+++ Array&String/isUnique.cpp
@@ -1,1 +1,29 @@
-Heloo
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+bool isUnique(const string &str)
+{
+
+    for (int i = 0; i < str.length() - 1; i++)
+    {
+        for (int j = i + 1; j < str.length() - 1; j++)
+        {
+            if (str[i] == str[j])
+            {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+int main()
+{
+    vector<string> words = {"abcde", "hello", "apple", "kite", "padle"};
+    for (auto word : words)
+    {
+        cout << word << isUnique(word) << endl;
+    }
+}
---
+++ build/CMakeFiles/New.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/New.dir/New.cpp.obj"
+  "libNew.pdb"
+  "libNew.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/New.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
